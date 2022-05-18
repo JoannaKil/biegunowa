c39ecc78ba6fab91e304b7890a3ae8556710bfdd
Asia <1105jelly@gmail.com>
2022-05-18 22:54:29 +0200

    update 18.05

--- projekt/main.py
+++ projekt/main.py
@@ -1,7 +1,6 @@
 # ten kod to nie jest jakis super, na razie jest tak zebym to ja rozumiala co sie tu dzieje
 import numpy as np
-import pandas as pd
-import matplotlib as plt
+from matplotlib import pyplot as plt
 g = 9.81
 h = float(input('Podaj wysokosc [m] = '))
 ro = 1.225 * ((1 - (h / 44300)) ** 4.256)
@@ -13,7 +12,7 @@ print('Masa srednia = ' + str(m_sr) + ' [kg]')
 S = float(input('Podaj powierzchnie nosna = '))
 Cz_max = float(input('Podaj maksymalny wspolczynnik sily nosnej = '))
 V_max = float(input('Podaj predkosc maksymalna [m/s] = '))
-Cz_n = (2 * m_max * g)/(ro * S * (V_max ** 2))
+Cz_n = (1/3)*(2 * m_max * g)/(ro * S * (V_max ** 2))
 print('Cz_n = ' + str("{:.4f}".format(Cz_n)))
 Cx_0 = float(input('Podaj Cx0 = '))
 Ae = float(input('Podaj wydluzenie efektywne = '))
@@ -23,17 +22,17 @@ z = list(np.arange(Cz_n, Cz_max, 0.01))
 z.append(Cz_max)
 x = []
 Cx = 0
-Vx = []
+Vx = []     # dla masy max
 V_x = 0
-Vn = []
+Vn = []     # dla masy minimalnej
 V_n = 0
-Vs = []
+Vs = []     # dla masy sredniej
 V_s = 0
-wx = []
+wx = []     # dla masy max
 w_x = 0
-wn = []
+wn = []     # dla masy minimalnej
 w_n = 0
-ws = []
+ws = []     # dla masy sredniej
 w_s = 0
 gam = []
 gamma = 0
@@ -55,4 +54,16 @@ for i in z:
     wn.append(w_n)
     w_s = ((2 * m_sr * g * (Cx ** 2)) / (ro * S * (((i ** 2) + (Cx ** 2)) ** 1.5))) ** 0.5
     ws.append(w_s)
-print(Vs)
+"""
+print(z)
+print(x)
+print(Vx)
+print(wx)
+"""
+plt.plot(Vx, wx, label="masa maksymalna")
+plt.plot(Vn, wn, label="masa minimalna")
+plt.plot(Vs, ws, label="masa srednia")
+plt.xlabel("V [m/s]")
+plt.ylabel("w [m/s]")
+plt.legend()
+plt.show()
